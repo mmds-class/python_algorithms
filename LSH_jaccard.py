d6ef098eff1f016a20d72b389326fdb478bf3903
Derek Farren <dfarren@gmail.com>
2014-10-16 14:43:18 -0700

    added column sorting

--- LSH_jaccard.py
+++ LSH_jaccard.py
@@ -6,7 +6,6 @@ Created on Oct 8, 2014
 import sys
 import math
 from collections import defaultdict
-import pdb
 import random
 
 
@@ -43,21 +42,23 @@ class HashFunc(object):
     
     
 def minhash(doc_shingles_dct, all_shingles_lst, hash_func_lst):
-    #this can be speeded up with numpy
     nbr_permutations = len(hash_func_lst)
     doc_signature_dct =  defaultdict(lambda:[sys.maxint]*nbr_permutations) 
-    
+    #sort all columns
+    print "\tsorting columns..."
+    doc_column_dct = sort_documents(doc_shingles_dct, all_shingles_lst)
     #for each row
-    for i, shingle in enumerate(all_shingles_lst):
+    print "\ttraversing rows..."
+    for i in xrange(len(all_shingles_lst)):
         permutation_lst = []
         #Compute all permutations for this row and save them to be used by all columns. 
         #This is important since it avoids computing these permutations again for each column.
         for hash_func in hash_func_lst:
             permutation_lst.append(hash_func(i))
         #for each column
-        for doc_id, shingles_dct in doc_shingles_dct.iteritems():
+        for doc_id, column_lst in doc_column_dct.iteritems():
             #if this row has a 1
-            if shingle in shingles_dct:
+            if column_lst[i]:
                 #update signature values
                 for j, permutation in enumerate(permutation_lst):                    
                     doc_signature_dct[doc_id][j] = min(permutation, doc_signature_dct[doc_id][j])
@@ -65,6 +66,41 @@ def minhash(doc_shingles_dct, all_shingles_lst, hash_func_lst):
     return dict(doc_signature_dct)
 
 
+def sort_documents(doc_shingles_dct, all_shingles_lst):
+    '''
+    Create columns for all documents
+    Here columns are created as sparse lists in order to follow the class explanation. A faster and more scalable way is to create min heap with the row values for each shingle in the doc.
+    That way in the min hash we traverse rows and get the min heap value on each document. If the min heap is equal to the current row, then it's live the row value for that doc is 1.
+    '''
+    doc_binary_dct = {}
+    i=0
+    for doc_id, shingles_lst in doc_shingles_dct.iteritems():
+        sorted_binary_lst = sort_document(shingles_lst, all_shingles_lst)
+        doc_binary_dct[doc_id] = sorted_binary_lst
+        i+=1
+        if i%1000==0:
+            print "\t\tsorted %i documents" % i
+    
+    return doc_binary_dct 
+        
+def sort_document(shingles_lst, all_shingles_lst):
+    '''create list of bytes representing each row in O(n) time'''
+    sorted_binary_lst = []
+    shingles_lst.sort()
+    j=0
+    for shingle in all_shingles_lst:
+        if j<len(shingles_lst):
+            if shingle!=shingles_lst[j]:
+                sorted_binary_lst.append(False)
+            else:            
+                sorted_binary_lst.append(True)
+                j+=1
+        else:
+            sorted_binary_lst.append(False)
+    
+    return sorted_binary_lst                
+    
+    
 def LSH(doc_signature_dct, nbr_of_bands):
     '''locality sensitive hashing dividing signatures into bands'''
     nbr_of_permutations = len(doc_signature_dct.itervalues().next())
@@ -88,13 +124,13 @@ def LSH(doc_signature_dct, nbr_of_bands):
 
 
 def shingles_in_lst(word_lst, k):
-    shingles_dct = {}
+    shingles_set = set()
     for i in xrange(len(word_lst)-k+1):
         shingle = ''
         for j in xrange(k):
             shingle+=word_lst[i+j]
-        shingles_dct[shingle]=None
-    return shingles_dct
+        shingles_set.add(shingle)
+    return list(shingles_set)
 
 
 def dict_from_file(file_location, k):
@@ -105,11 +141,14 @@ def dict_from_file(file_location, k):
         for line in f:      
             idx = line.find(' ')
             doc_id = line[:idx]
-            shingles_dct = shingles_in_lst(line[idx+1:].split(), k)
-            doc_shingles_dct[doc_id] = shingles_dct 
-            for shingle in shingles_dct.iterkeys():
+            shingles_lst = shingles_in_lst(line[idx+1:].split(), k)
+            doc_shingles_dct[doc_id] = shingles_lst 
+            for shingle in shingles_lst:
                 shingles_set.add(shingle)
-    return doc_shingles_dct, list(shingles_set)
+    
+    all_shingles_lst = list(shingles_set)
+    all_shingles_lst.sort()
+    return doc_shingles_dct, all_shingles_lst
 
 
 def look_for_pairs(buckets_dct_lst):
@@ -149,11 +188,10 @@ def process_docs(file_location, k, nbr_of_bands):
 
 if __name__=='__main__':
     file_location = 'documents.txt'
-    k = 3
+    k = 3 #k value in k-gram for shingles
     nbr_of_bands = 3
     rows_per_band = 8
     nbr_permutations = nbr_of_bands*rows_per_band
     pairs_lst = process_docs(file_location, k, nbr_of_bands)
     print "Found %i probable duplicated documents" % len(pairs_lst)
-    
     
